8514d24edaf64c4b60d4b484b732364b032acd58
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-08-20 16:58:21 -0700

    final JS

--- class-14/lab/starter-code/js/cart.js
+++ class-14/lab/starter-code/js/cart.js
@@ -19,18 +19,30 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  table.innerHTML = ''; 
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
+  var tbody = document.getElementById('cart'); 
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
+  for (var i = 0; i < cart.length; i++){
+    // TODO: Create a TR
+    var tableRow = document.createElement('tr');
+    // TODO: Create a TD for the delete link, quantity, and the item
+    var tableData = document.createElement('td');
+    var deleteButton= tableData.textContent = ('X'); // cart.html refereced clicking an X
+    var quantity = tableData.textContent = (event.target.quantity.value);
+    var productName = tableData.textContent = (name);
+  }
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
-
+  tbody.appendChild(tableRow);
+  tableRow.appendChild(deleteButton);
+  tableRow.appendChild(quantity);
+  tableRow.appendChild(productName);
 }
 
 function removeItemFromCart(event) {
@@ -38,6 +50,7 @@ function removeItemFromCart(event) {
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
+  showCart();
 
 }
 
--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -30,8 +30,8 @@ for(var i=0;i <Product.allProducts.length; i++){
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-  event.preventDefault();
   // TODO: Prevent the page from reloading
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -44,12 +44,13 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
-  if (name === Product.allProducts[i].name){
-    return event.target.quanity.value
-    Cart.push(Product.allProducts[i].name)
+  var picked = document.getElementById('items').value;
   // TODO: get the quantity
+  var quantity = document.getElementById('quantity').value;
   // TODO: using those, add one item to the Cart
-}
+  cart.addItem(picked,quantity);
+  }
+
 // TODO: Update the cart count in the header nav with the number of items in the Cart
 function updateCounter() {}
 
@@ -60,12 +61,11 @@ function updateCartPreview() {
 }
 
 // Set up the "submit" event listener on the form.
-
-// This is the trigger for the app. When a user "submits" the form, it will
-// Call that handleSubmit method above and kick off the whole process
 var catalogForm = document.getElementById('catalog');
 catalogForm.addEventListener('submit', handleSubmit);
+// This is the trigger for the app. When a user "submits" the form, it will
+// Call that handleSubmit method above and kick off the whole process
 
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
-populateForm();
+populateForm(); 
